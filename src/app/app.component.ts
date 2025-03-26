615e97dcf2665fd32cee9fb696f4ca1f1d8f5827
Ramdhan Vanjara <ramdhanV@odek.co.in>
2025-03-26 13:00:41 +0530

    made count project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+
+  handleClickEvent() {
+    console.log("Function Called");
+    this.otherFunction
+  }
+
+  otherFunction() {
+    console.log("Other Function Called");
+  }
+
+  count: number = 0
+
+  handleCount(val: string) {
+    if (val == 'mins' && this.count!=0) {
+      this.count = this.count - 1
+    }
+    else if (val == 'plus') {
+      this.count = this.count + 1
+    }
+    else {
+      this.count = 0
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
