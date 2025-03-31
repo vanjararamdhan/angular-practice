c72e92c002c006f61e851236ed7d4b54783d5bd0
Ramdhan Vanjara <ramdhanV@odek.co.in>
2025-03-31 12:02:51 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,4 +30,60 @@ export class AppComponent {
       this.count = 0
     }
   }
+
+  display=true
+  x=3
+
+  hide(){
+    this.display = false
+  }
+
+  show(){
+    this.display = true
+  }
+
+  toggele(){
+    this.display = !this.display
+  }
+// ------------------------------------------------------------------------------------------------------------
+
+users = ['Ram', 'Syam', 'Ghansyam']
+
+students = [
+  {
+    id: 1,
+    name: 'Ram',
+    age: 20,
+    grade: 'A',
+    email: 'ram@example.com',
+    phone: '123-456-7890',
+    address: '123 Main St, Surat'
+  },
+  {
+    id: 2,
+    name: 'Syam',
+    age: 21,
+    grade: 'B',
+    email: 'syam@example.com',
+    phone: '987-654-3210',
+    address: '456 Park Ave, Ahmedabad'
+  },
+  {
+    id: 3,
+    name: 'Ghansyam',
+    age: 22,
+    grade: 'A+',
+    email: 'ghansyam@example.com',
+    phone: '555-123-4567',
+    address: '789 Elm St, Vadodara'
+  }
+];
+
+
+// --------------------------------------------------Effect with signal----------------------------------------------------------
+// userName = signal('Ram') 
+
+
+// ------------------------------------------------------------------------------------------------------------
+
 }
